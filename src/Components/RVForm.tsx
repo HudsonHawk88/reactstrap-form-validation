5ee393d9d7a35a8c5b98ed5ec4f2e3234f93b7d8
HudsonHawk88 <toth.gergo88@hotmail.com>
2022-05-20 18:55:19 +0200

    feat(reactstrap-form-validation): add files

---
+++ src/Components/RVForm.tsx
@@ -0,0 +1,82 @@
+import { FC, ReactElement, ChangeEvent, useState } from "react";
+import {
+  Form,
+  Input,
+  FormGroup,
+  FormFeedback,
+  FormProps,
+  InputProps,
+  FormGroupProps,
+  FormFeedbackProps,
+} from "reactstrap";
+import "bootstrap/dist/css/bootstrap.min.css";
+
+const RVForm: FC<FormProps> = ({
+  className = "reactstrap-form",
+  children,
+  onSubmitForm,
+  ...rest
+}): ReactElement => {
+  return (
+    <Form className={className} {...rest}>
+      {children}
+    </Form>
+  );
+};
+
+const RVFormGroup: FC<FormGroupProps> = ({
+  className = "reactstrap-form-group",
+  children,
+  ...rest
+}): ReactElement => {
+  return (
+    <FormGroup className={className} {...rest}>
+      {children}
+    </FormGroup>
+  );
+};
+
+const RVFormFeedback: FC<FormFeedbackProps> = ({
+  className = "reactstrap-form-feedback",
+  children,
+  ...rest
+}): ReactElement => {
+  return (
+    <FormFeedback className={className} {...rest}>
+      {children}
+    </FormFeedback>
+  );
+};
+
+const RVInput: FC<InputProps> = ({
+  className = "reactstrap-form-input",
+  onChange = () => {
+    console.log("...");
+  },
+  ...rest
+}): ReactElement => {
+  const [isValid, setIsValid] = useState(true);
+
+  const validate = (e: ChangeEvent<HTMLInputElement>) => {
+    if (e.target.value === "") {
+      setIsValid(false);
+    } else {
+      setIsValid(true);
+    }
+  };
+
+  return (
+    <Input
+      onChange={(e: ChangeEvent<HTMLInputElement>) => {
+        onChange(e);
+        validate(e);
+      }}
+      onInvalid={(e: ChangeEvent<HTMLInputElement>) => validate(e)}
+      invalid={!isValid}
+      className={className}
+      {...rest}
+    />
+  );
+};
+
+export { RVForm, RVFormGroup, RVInput, RVFormFeedback };
